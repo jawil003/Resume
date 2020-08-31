8f0430e2556064173b34b1556d33c8449aa0890d
Jannik <jw19980309@gmail.com>
2020-08-31 21:56:04 +0200

    Updated WebComponents

--- components/cover.ts
+++ components/cover.ts
@@ -1,7 +1,20 @@
-class Cover extends HTMLImageElement {
+class ImageCover extends HTMLElement {
   constructor() {
     super();
+    this.config();
+    this.render();
+  }
+  config() {
+    this.attachShadow({ mode: "open" });
+  }
+  render() {
+    if (this.shadowRoot)
+      this.shadowRoot.innerHTML = `
+        <div style="display: flex; justify-content: center; width: 100vw; height: 20vh; position: absolute; top: 0px; left: 0px;"><img width="100%" height="100%" src="${this.getAttribute(
+          "src"
+        )}" /></div>
+      `;
   }
 }
 
-customElements.define("cover", Cover);
+customElements.define("image-cover", ImageCover);
--- components/custom-h1.ts
+++ components/custom-h1.ts
@@ -1,6 +1,8 @@
-class CustomH1 extends HTMLHeadingElement {
+class CustomH1 extends HTMLElement {
   constructor() {
     super();
+    this.config();
+    this.render();
   }
   config() {
     this.attachShadow({ mode: "open" });
@@ -8,9 +10,9 @@ class CustomH1 extends HTMLHeadingElement {
   render() {
     if (this.shadowRoot)
       this.shadowRoot.innerHTML = `
-        <h1 style="color:blue">${this.getAttribute("innerHtml")}<h1>
+        <h1 style="color:var(--blue)"><slot></slot><h1>
       `;
   }
 }
 
-customElements.define("h1", CustomH1);
+customElements.define("custom-h1", CustomH1);
